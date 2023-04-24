a7560a6f3ec821732db96905e5f8199041dd22a6
Altealien <omotayodavid657@gmail.com>
2023-04-24 18:25:55 +0100

    modified:   .vscode/settings.json 	modified:   Chapter 3/Invoice.cpp

--- Chapter 3/Invoice.cpp
+++ Chapter 3/Invoice.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
-#include <string>
 #include "Invoice.h"
 
 int main(){
-    Invoice myInvoice("ABC10","Car paint",1000, 8);
-    std::cout << "The part number is " << myInvoice.getPartNumber() << std::endl;
-    std::cout << "The description is " << myInvoice.getDescription() << std::endl;
-    std::cout << "The invoice is $" << myInvoice.getInvoiceAmount() << std::endl;
-    return 0;
+    Invoice invoice1("ABC100", "Widget A", 4, 10);
+    // Test getters
+    std::cout << "Invoice number: " << invoice1.getPartNumber() << std::endl;
+    std::cout << "Description: " << invoice1.getDescription() << std::endl;
+    std::cout << "Price per item: $" << invoice1.getPrice() << std::endl;
+    std::cout << "Quantity: " << invoice1.getQuantity() << std::endl;
+    std::cout << "Invoice amount: $" << invoice1.getInvoiceAmount() << std::endl;
+
+    // Test setters
+    invoice1.setPartNumber("DEF456");
+    invoice1.setDescription("Widget B");
+    invoice1.setPrice(12);
+    invoice1.setQuantity(3);
+
+    // Test getters again
+    std::cout << "\n";
+    std::cout << "Invoice number: " << invoice1.getPartNumber() << std::endl;
+    std::cout << "Description: " << invoice1.getDescription() << std::endl;
+    std::cout << "Price per item: $" << invoice1.getPrice() << std::endl;
+    std::cout << "Quantity: " << invoice1.getQuantity() << std::endl;
+    std::cout << "Invoice amount: $" << invoice1.getInvoiceAmount() << std::endl;
+    std::cout <<"\n";
+
+
 }
